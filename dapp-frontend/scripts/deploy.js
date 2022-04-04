8e12bce643a3d971e7cc481cc47c33704b774bea
Cristi <thiess.cristi@gmail.com>
2022-04-04 19:17:52 +0300

    reverted a change

--- dapp-frontend/scripts/deploy.js
+++ dapp-frontend/scripts/deploy.js
@@ -27,17 +27,14 @@ async function deployFileManager() {
   return fileManager.address;
 }
 
-async function deployPostsManager() {
+async function main() {
+  /* these two lines deploy the contract to the network */
   const PostsManager = await hre.ethers.getContractFactory("PostsManager");
   const postsManager = await PostsManager.deploy();
 
   await postsManager.deployed();
   console.log("PostsManager deployed to:", postsManager.address);
 
-  return postsManager.address, postsManager.signer.address;
-}
-
-async function deployUserProfileManager() {
   const UserProfileManager = await hre.ethers.getContractFactory(
     "UserProfileManager"
   );
@@ -45,26 +42,18 @@ async function deployUserProfileManager() {
   await userProfileManager.deployed();
   console.log("UserProfileManager deployed to:", userProfileManager.address);
 
-  return userProfileManager.address;
-}
-
-async function main() {
-  /* these two lines deploy the contract to the network */
-
   /* this code writes the contract addresses to a local */
   /* file named config.js that we can use in the app */
 
-  const [postsManagerAddress, ownerAddress] = await deployPostsManager();
-  const userProfileManagerAddress = await deployUserProfileManager();
   const requestManagerAddress = await deployRequestManager();
   const fileManagerAddress = await deployFileManager();
 
   fs.writeFileSync(
     "./config.js",
     `
-  export const postsManagerAddress = "${postsManagerAddress}"
-  export const ownerAddress = "${ownerAddress}"
-  export const userProfileManagerAddress ="${userProfileManagerAddress}"
+  export const postsManagerAddress = "${postsManager.address}"
+  export const ownerAddress = "${postsManager.signer.address}"
+  export const userProfileManagerAddress ="${userProfileManager.address}"
   export const requestManagerAddress="${requestManagerAddress}"
   export const fileManagerAddress="${fileManagerAddress}"
   `
